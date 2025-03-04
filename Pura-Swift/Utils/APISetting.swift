e69a0635aabacc50ee6df88a521a610bff6b5492
Sasutora <112454542+Sasutora@users.noreply.github.com>
2025-03-04 12:29:24 +0700

    Update APISetting.swift

--- Pura-Swift/Utils/APISetting.swift
+++ Pura-Swift/Utils/APISetting.swift
@@ -8,6 +8,6 @@
 struct API {
     // ganti url sesuai BEnya jika mengetest menggunakan iphone gunakan ip address local dari komputernya
 //    static let baseURL = "http://YOUR_LOCAL_IP:3000"
-    static let baseURL = "http://192.168.1.5:3000"
+    static let baseURL = "http://192.168.1.6:3000"
     
 }
